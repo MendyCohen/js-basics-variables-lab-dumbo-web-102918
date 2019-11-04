288c3f6c00e04469def41c0e35e5c7d799e93072
Mendy Cohen <mendybeltcohen@gmail.com>
2019-11-04 05:58:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solution in this file!
-let companyName = 'Scuber';
+const companyName = 'Scuber';
+companyName = "specialCompany";
 let mostProfitableNeighborhood = "Chelsea";
+mostProfitableNeighborhood = "Upper West Side";
 let companyCeo = "Susan Smith";
+companyCeo = "Lauren Hart";
