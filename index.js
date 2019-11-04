d5351cdec0d795a9731ceaf05c250a5d04176409
Mendy Cohen <mendybeltcohen@gmail.com>
2019-11-04 05:58:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const companyName = 'Scuber';
 companyName = "specialCompany";
 let mostProfitableNeighborhood = "Chelsea";
-mostProfitableNeighborhood = "Upper West Side";
+
 let companyCeo = "Susan Smith";
 companyCeo = "Lauren Hart";
